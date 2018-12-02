f2eaa097f637d22b182c87f4412945e8f1391d97
DESKTOP-QKAVK6G\Mark <mwillson@oswego.edu>
2018-12-01 23:05:10 -0800

    Clicking vertices removes them!!!

---
+++ Assets/Scripts/HighlightVertex.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightVertex : MonoBehaviour {
+
+	TestPolygon polygon;
+	[SerializeField]
+	public int removalIndex;
+	public bool isHighlighting;
+
+	// Use this for initialization
+	void Start () {
+		polygon = GameObject.FindObjectOfType<TestPolygon> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetHighlightedVertex(Vector3 v){
+		removalIndex = polygon.verticesList.FindIndex (vec => vec.x == v.x && vec.y == v.y && vec.z == v.z);
+	}
+}
---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerController : MonoBehaviour {
+
+	float zDepth;
+	Transform polygon;
+	GameObject highlightGO;
+	TestPolygon polygonScript;
+	bool highlightedOne;
+
+	// Use this for initialization
+	void Start () {
+		polygon = GameObject.Find ("TestPolygon").transform;
+		//mainCam = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		polygonScript = polygon.GetComponent<TestPolygon>();
+		highlightGO = transform.Find ("Highlighter").gameObject;
+		highlightedOne = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//the higher the polygon's z value, the farther it is from the camera
+		zDepth = polygon.position.z + 10;
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = zDepth;
+		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+		//Debug.Log ("mouse: " + mousePos);
+		//set this transform's position to the mouse's world position
+		transform.position = mousePos;
+		//check if near a polygon vertex
+		highlightedOne = false;
+		foreach (Vector3 vert in polygonScript.verticesList) {
+			if (Vector3.Distance (mousePos, vert) < .25f) {
+				highlightedOne = true;
+				highlightGO.GetComponent<SpriteRenderer> ().enabled = true;
+				highlightGO.GetComponent<HighlightVertex> ().SetHighlightedVertex (vert);
+				highlightGO.GetComponent<HighlightVertex> ().isHighlighting = true;
+			}
+		}
+		//if we were not near any vertex, disable highlighter this frame
+		if (!highlightedOne) {
+			highlightGO.GetComponent<SpriteRenderer> ().enabled = false;
+		}
+	}
+
+	void OnMouseDown(){
+		//if highlighter is highlighting something
+		//remove the vertex its highlighting
+		if (highlightedOne) {
+			Debug.Log ("clicked while highlighted");
+
+			polygonScript.RemoveVertex(highlightGO.GetComponent<HighlightVertex>().removalIndex);
+		}
+	}
+}
